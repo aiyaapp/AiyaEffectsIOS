b27fec270a9c575e6d2826d34970fadf2aea2f45
汪洋 <wangyang@aiyaapp.com>
2018-03-26 10:20:56 +0800

    1. add gaussianBlurFilter

---
+++ AiyaEffectSDK/AiyaEffectSDK/AyGPUImageCustomFilter/AYGPUImageGaussianBlurFilter.h
@@ -0,0 +1,15 @@
+//
+//  AYGPUImageGaussianBlurFilter.h
+//  AiyaEffectSDK
+//
+//  Created by 汪洋 on 2018/3/16.
+//  Copyright © 2018年 深圳市哎吖科技有限公司. All rights reserved.
+//
+
+#import "AYGPUImageFilter.h"
+
+@interface AYGPUImageGaussianBlurFilter : AYGPUImageFilter
+
+@property (nonatomic,assign) CGFloat blurRadiusInPixels;
+
+@end
